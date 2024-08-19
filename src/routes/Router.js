484b6a038a29d294c4c5f234efa1b41361e14786
Luvous <thomasvarela2001@outlook.es>
2024-08-18 22:43:43 -0300

    Sitemap fix with router

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {
     BrowserRouter as Router,
     Routes,
-    Route
+    Route,
+    useLocation
 } from "react-router-dom";
 import Home from './Home.jsx';
 import Services from './Services.jsx';
@@ -20,11 +21,26 @@ const Webpages = () => {
                 <Route exact path="/contact" element={<Contact />} />
                 <Route exact path="/contact/sent" element={<SentContact />} />
                 <Route exact path="/about" element={<About />} />
- 
-                <Route path="*" element={<Page404 />} /> 
+                
+                <Route path="*" element={<Page404Handler />} /> 
             </Routes>
         </Router>
     );
 };
+
+// Componente que maneja la lógica de excepción para /sitemap
+const Page404Handler = () => {
+    const location = useLocation();
+
+    // Verifica si la URL es /sitemap y si es así, no renderiza el componente de 404
+    if (location.pathname === "/sitemap") {
+        window.location.href = "/sitemap.xml";
+        return null;
+    }
+
+    return <Page404 />;
+};
+
+
 
 export default Webpages;
